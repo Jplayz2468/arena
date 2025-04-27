aebec6e456abe766ae01f5c1fc0b6b2c429af2d2
Jplayz2468 <impressiveplayer2468@gmail.com>
2025-04-27 16:16:17 -0700

    fixed multiplayer (again)

--- game.gd
+++ game.gd
@@ -2,38 +2,56 @@ extends Node2D
  
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
-@export var server_port: int = 135
-@export var server_address: String = ""  # Will be set via UI
+@export var host_port: int = 135  # Port used when hosting
+@export var remote_address: String = ""  # Set this in inspector when joining
+@export var remote_port: int = 0  # Remote port (different from host port, set in inspector)
 
-# Add UI elements for server address input
-@onready var address_input = $AddressInput  # Add this LineEdit to your scene
+# Simple connection status
+var connection_status = "Not connected"
 
 func _on_host_pressed():
-	# Same as before, create a server on the specified port
-	peer.create_server(server_port)
+	# Create server on host_port
+	var error = peer.create_server(host_port)
+	if error != OK:
+		print("Server creation failed with error: ", error)
+		return
+		
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(_add_player)
-	_add_player()
+	multiplayer.peer_disconnected.connect(_remove_player)
 	
-	# Display connection info to the host
-	print("Server started on port: ", server_port)
-	print("Give clients this address: Your tunnel URL + port ", server_port)
+	print("Server running on port ", host_port)
+	_add_player()
 
 func _add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
 	call_deferred("add_child", player)
+	
+func _remove_player(id):
+	if has_node(str(id)):
+		get_node(str(id)).queue_free()
 
 func _on_join_pressed():
-	# Parse the input which might be in format "address:port"
-	var address_parts = address_input.text.split(":")
+	# Connect to the address and port set in the inspector
+	if remote_address.is_empty():
+		print("Remote address not set in inspector!")
+		return
+		
+	if remote_port <= 0:
+		print("Invalid remote port set in inspector!")
+		return
 	
-	server_address = address_parts[0]  # "8.tcp.us-cal-1.ngrok.io"
+	print("Connecting to ", remote_address, ":", remote_port)
 	
-	# If port is specified in the address, use that instead of default
-	if address_parts.size() > 1:
-		server_port = int(address_parts[1])  # 16556
-	
-	print("Connecting to: ", server_address, ":", server_port)
-	peer.create_client(server_address, server_port)
+	var error = peer.create_client(remote_address, remote_port)
+	if error != OK:
+		print("Client creation failed with error: ", error)
+		return
+		
 	multiplayer.multiplayer_peer = peer
+	
+	# Add connection status signals with simple print statements
+	multiplayer.connected_to_server.connect(func(): print("Connected successfully!"))
+	multiplayer.connection_failed.connect(func(): print("Connection failed!"))
+	multiplayer.server_disconnected.connect(func(): print("Server disconnected!"))
